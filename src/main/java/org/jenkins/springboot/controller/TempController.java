a1a16a2604d1909dab167b48df9c59788884dcc6
alper.kurtoglu <alper.kurtoglu@tubitak.gov.tr>
2023-03-20 16:27:46 +0300

    COMMIT-1

--- src/main/java/org/jenkins/springboot/controller/TempController.java
+++ src/main/java/org/jenkins/springboot/controller/TempController.java
@@ -1,2 +1,13 @@
-package org.jenkins.springboot.controller;public class TempController {
+package org.jenkins.springboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TempController {
+
+    @GetMapping("/temp")
+    public String temp() {
+        return "Request received!!";
+    }
 }
