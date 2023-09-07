11d12bb1180b7f5229d463f5dee834eac299d477
alper.kurtoglu <alper.kurtoglu@tubitak.gov.tr>
2023-09-07 21:27:06 +0300

    COMMIT-5

--- vars/triggerProject.groovy
+++ vars/triggerProject.groovy
@@ -1,5 +1,11 @@
 def call() {
         def timestampVersion = sh(script: "date -d @\$(date +%s) +'%Y-%m-%dT%H-%M-%SZ'", returnStdout: true).trim()
+        def IS_CRON_JOB = "${currentBuild.rawBuild.getCause(hudson.triggers.TimerTrigger.TimerTriggerCause) != null ? 'true' : 'false'}"
 
-        sh "echo cronjob ${timestampVersion}!!!"
+//        if (IS_CRON_JOB.toBoolean()) {
+//                sh ' ./gradlew :apps:urun:jib -Djib.to.tags=$timestampVersion'
+//        } else {
+//                sh ' ./gradlew :apps:urun:jib'
+//        }
+        sh "echo cronjob ${IS_CRON_JOB}!!!"
 }
