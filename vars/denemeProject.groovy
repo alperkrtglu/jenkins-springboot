0ed96e80aa19c9751f99bb72180c83c6de318bbc
alper.kurtoglu <alper.kurtoglu@tubitak.gov.tr>
2023-09-13 15:35:58 +0300

    COMMIT-5

--- vars/denemeProject.groovy
+++ vars/denemeProject.groovy
@@ -1,7 +1,7 @@
 def call(Map config = [:]) {
-        sh "echo Halloo ${config.isim}!!!"
+        sh "echo ${config.isim}"
 
         if (config.isim == 'ALPER') {
-                sh 'echo ${config.isim} OLDU GALIBA'
+                sh "echo ${config.isim} OLDU GALIBA"
         }
 }
