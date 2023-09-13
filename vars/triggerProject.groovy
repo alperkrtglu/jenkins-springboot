85d001f67302a57e11d2a944ff24063d49b73920
alper.kurtoglu <alper.kurtoglu@tubitak.gov.tr>
2023-09-13 16:32:07 +0300

    COMMIT-5

--- vars/triggerProject.groovy
+++ vars/triggerProject.groovy
@@ -1,6 +1,6 @@
 def call(Map config = [:]) {
 
-        if (config.IS_CRON_JOB) {
+        if (config.IS_CRON_JOB == 'TRUE') {
                 sh "echo RUN-CRONJOB --- TIME ${config.timestampVersion}"
         } else {
                 sh "echo RUN-MANUAL --- TIME ${config.timestampVersion}"
